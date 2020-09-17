f922656ddae02d29719adaa17a33ba136fdd7263
Eiríkur Friðjón Kjartansson <eirikurfk@gmail.com>
2020-09-17 10:31:45 +0000

    Created available directions function

--- tiletraveller.py
+++ tiletraveller.py
@@ -1,4 +1,8 @@
 # Tile Traveller
+# E: +1 to x (unless 1,1 eða ef x = 3)
+# S: -1 to y (unless 2,3 eða ef y = 1)
+# W: -1 to x (unless 3,2 eða ef x = 1)
+# N: +1 to y (unless 2,2 eða ef y = 3)
 
 def move(direction):
     if direction == "e":
@@ -14,12 +18,32 @@ def move(direction):
         locationy += -1
         return locationy
 
+def available_directions(locationx, locationy):
+    if locationx == 1:
+        if locationy == 1:
+            print("You can travel: (N)orth.")
+        elif locationy == 3:
+            print("You can travel: (E)ast and (S)outh.")
+        else:
+            print("You can travel: (E)ast, (N)orth and (S)outh.")
+    elif locationx == 2:
+        if locationy == 1:
+            print("You can travel: (N)orth.")
+        elif locationy == 2:
+            print("You can travel: (W)est and (S)outh.")
+        else:
+            print("You can travel: (W)est and (East)")
+
+
+
+
+
 locationx = 1
 locationy = 1
 
 
 while location != 3,1:
 
-dircetion = input("Direction: ")
+direction = input("Direction: ")
 
 movement = move(direction)
