62e8160aaea6dc5de927066b4ac7c3741cf884d7
Gudbrandur Nói <guddbrandur99mail.com>
2020-09-17 11:01:12 +0000

    föll og fleira

--- tiletraveller.py
+++ tiletraveller.py
@@ -4,6 +4,40 @@
 # W: -1 to x (unless 3,2 eða ef x = 1)
 # N: +1 to y (unless 2,2 eða ef y = 3)
 
+def available_directions(locationx, locationy):
+    if locationx == 1:
+        if locationy == 1:
+            print("You can travel: (N)orth.")
+            available = "N"
+        elif locationy == 3:
+            print("You can travel: (E)ast and (S)outh.")
+            available = "ES"
+        else:
+            print("You can travel: (E)ast, (N)orth and (S)outh.")
+            available = "ENS"
+    elif locationx == 2:
+        if locationy == 1:
+            print("You can travel: (N)orth.")
+            available = "N"
+        elif locationy == 2:
+            print("You can travel: (W)est and (S)outh.")
+            available = "WS"
+        else:
+            print("You can travel: (W)est and (E)ast")
+            available = "WE"
+    elif locationx == 3:
+        if locationy == 1:
+            print("You can travel: (N)orth.")
+            available = "N"
+        elif locationy == 2:
+            print("You can travel: (N)orth and (S)outh.")
+            available = "NS"
+        else:
+            print("You can travel: (W)est and (S)outh")
+            available = "WS"
+    
+    return available
+
 def move(direction):
     if direction == "e":
         locationx += 1
@@ -17,22 +51,8 @@ def move(direction):
     elif direction == "s":
         locationy += -1
         return locationy
-
-def available_directions(locationx, locationy):
-    if locationx == 1:
-        if locationy == 1:
-            print("You can travel: (N)orth.")
-        elif locationy == 3:
-            print("You can travel: (E)ast and (S)outh.")
-        else:
-            print("You can travel: (E)ast, (N)orth and (S)outh.")
-    elif locationx == 2:
-        if locationy == 1:
-            print("You can travel: (N)orth.")
-        elif locationy == 2:
-            print("You can travel: (W)est and (S)outh.")
-        else:
-            print("You can travel: (W)est and (East)")
+    
+    
 
 
 
@@ -41,9 +61,10 @@ def available_directions(locationx, locationy):
 locationx = 1
 locationy = 1
 
-
-while location != 3,1:
-
-direction = input("Direction: ")
-
-movement = move(direction)
+while locationx != 3 and locationy != 1:  
+  available_directions(locationx, locationy)
+  direction = input("Direction: ")
+  if direction in available_directions():
+    movement = move(direction)
+  else:
+    print("Not a valid direction!")
